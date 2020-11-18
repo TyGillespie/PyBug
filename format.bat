134837bd747ad90c04a9c134f9eecf917d64f497
Ty <60036684+TyGillespie@users.noreply.github.com>
2020-11-18 06:21:59 -0700

    Formatting.

---
+++ format.bat
@@ -0,0 +1,5 @@
+@echo off
+
+black pybug.py
+
+pause
--- pybug.py
+++ pybug.py
@@ -15,88 +15,88 @@ file = None
 
 
 def initialize(
-	file_name: str, overwrite: bool = True, log_startup: bool = True
+    file_name: str, overwrite: bool = True, log_startup: bool = True
 ) -> bool:
-	"""Initializes the log system.
-	Parems:
-			file_name: The log file name.
-			overwrite (optional): Specifies if the text that was already in the log gets overwritten or not.
-			log_startup (optional): If the message that the debugging system gets written to the log file.
-	Returns:
-			True if successful, False otherwise.
-	"""
-	global file
-	if file != None:
-		return False
-	if overwrite:
-		file = open(file_name, "w")
-	else:
-		file = open(file_name, "a")
-	if log_startup:
-		file.write(f"{datetime.datetime.now()}\n")
-		file.write("Logging system initialized.")
-	file.flush()
-	return True
+    """Initializes the log system.
+    Parems:
+            file_name: The log file name.
+            overwrite (optional): Specifies if the text that was already in the log gets overwritten or not.
+            log_startup (optional): If the message that the debugging system gets written to the log file.
+    Returns:
+            True if successful, False otherwise.
+    """
+    global file
+    if file != None:
+        return False
+    if overwrite:
+        file = open(file_name, "w")
+    else:
+        file = open(file_name, "a")
+    if log_startup:
+        file.write(f"{datetime.datetime.now()}\n")
+        file.write("Logging system initialized.")
+    file.flush()
+    return True
 
 
 def write_message(message: str) -> bool:
-	"""Writes a standard message to the log.
-	Parems:
-			message: The message to write.
-	Returns:
-			True on success, False otherwise.
-	"""
-	global file
-	if file == None:
-		return False
-	file.write(f"{datetime.datetime.now()}\n")
-	file.write(f"Message: {message}\n")
-	file.flush()
-	return True
+    """Writes a standard message to the log.
+    Parems:
+            message: The message to write.
+    Returns:
+            True on success, False otherwise.
+    """
+    global file
+    if file == None:
+        return False
+    file.write(f"{datetime.datetime.now()}\n")
+    file.write(f"Message: {message}\n")
+    file.flush()
+    return True
 
 
 def write_error(message: str) -> bool:
-	"""Writes an error to the log file.
-	Useful if you want to write an error that isn't an exception.
-	Parems:
-			message: The message to write.
-	Returns:
-			True upon success, False otherwise.
-	"""
-	global file
-	if file == None:
-		return False
-	file.write(f"{datetime.datetime.now()}\n")
-	file.write(f"Error: {message}\n")
-	file.flush()
+    """Writes an error to the log file.
+    Useful if you want to write an error that isn't an exception.
+    Parems:
+            message: The message to write.
+    Returns:
+            True upon success, False otherwise.
+    """
+    global file
+    if file == None:
+        return False
+    file.write(f"{datetime.datetime.now()}\n")
+    file.write(f"Error: {message}\n")
+    file.flush()
 
 
-def writeException(message: str) -> bool:
-	"""Writes an Exception to the log file.
-	Normally used in try/catch blocks.
-	Parems:
-			message: The message to write along with the Exception.
-	Returns:
-			True on success, False if it fails.
-	"""
-	global file
-	if file == None:
-		return False
-	file.write(f"{datetime.datetime.now()}\n")
-	file.write(f"Exception: {message}\n----\n{traceback.format_exc()}----\n")
-	debugFile.flush()
-	return True
+def write_exception(message: str) -> bool:
+    """Writes an Exception to the log file.
+    Normally used in try/catch blocks.
+    Parems:
+            message: The message to write along with the Exception.
+    Returns:
+            True on success, False if it fails.
+    """
+    global file
+    if file == None:
+        return False
+    file.write(f"{datetime.datetime.now()}\n")
+    file.write(f"Exception: {message}\n----\n{traceback.format_exc()}----\n")
+    file.flush()
+    return True
 
 
 def stop() -> bool:
-	"""Stops the logging system.
-	Parems:
-			None.
-	Returns:
-			True on success, False on fail.
-	"""
-	global file
-	if file == None:
-		return False
-	file.close()
-	return True
+    """Stops the logging system.
+    Parems:
+            None.
+    Returns:
+            True on success, False on fail.
+    """
+    global file
+    if file == None:
+        return False
+    file.close()
+    return True
